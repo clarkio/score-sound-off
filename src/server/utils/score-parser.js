1f8ec51f3959f8d9e1fac8dfcaeede090088ed6f
Clarkio <brian@clarkio.com>
2015-09-17 21:15:06 -0400

    fix for games that haven't started yet

--- src/server/utils/score-parser.js
+++ src/server/utils/score-parser.js
@@ -18,8 +18,12 @@ function parseScores(data, sport) {
 
 function cleanTheData (array) {
     array.forEach(function (item, index) {
-        // Game info starts after '=', clean up %20 between strings and add a | between teams        
-        array[index] = item.substring(item.indexOf('=') + 1).replace(/%20/g, ' ').replace('   ', '|');
+        // Game info starts after '=', clean up %20 between strings and add a | between teams
+        var replaceItem = '   ';
+        if (item.indexOf('at') > -1) {
+            replaceItem = ' at ';
+        }   
+        array[index] = item.substring(item.indexOf('=') + 1).replace(/%20/g, ' ').replace(replaceItem, '|');
     });
 }
 
@@ -37,7 +41,7 @@ function parseIntoGame (data) {
     var awayTeamParse = false;
     
     game.time = parseGameTime(data);
-    game.homeTeam = parseTeam(data, homeTeamParse);
+    game.homeTeam = parseTeam(data, homeTeamParse, game.time.active);
     game.awayTeam = parseTeam(data, awayTeamParse);
     
     if (!game.time.active) {
@@ -49,24 +53,32 @@ function parseIntoGame (data) {
 
 function parseGameTime (data) {
     var time = {};
-    time.clock = data.split('(').pop().split(')').shift();
     
-    if (time.clock === 'FINAL' || time.clock === 'FINAL - OT' ||
+    time.clock = data.split('(').pop().split(')').shift();
+    if (time.clock.indexOf('IN 1ST') > -1 || time.clock.indexOf('IN 2ND') > -1 || time.clock.indexOf('IN 3RD') > -1 ||
+            (time.clock.indexOf('IN 4TH') > -1 && time.clock !== '00:00 IN 4TH')) {
+        time.active = true;
+    } else {
+        if (time.clock === 'FINAL' || time.clock === 'FINAL - OT' ||
             time.clock === '00:00 IN 4TH' || time.clock === 'END OF 4TH' || 
             time.clock === '00:00 IN OT' || time.clock === '00:00 IN 2OT') {
-        time.clock = 'FINAL';
+                time.clock = 'FINAL';
+        }
         time.active = false;
-    } else {
-        time.active = true;
     }
     
     return time;
 }
 
-function parseTeam (data, isHomeTeam) {
+function parseTeam (data, isHomeTeam, isActive) {
     var team = {};
     var indexToTake = isHomeTeam ? 0 : 1;
-    var teamString = data.split('|')[indexToTake];
+    var teamString = data.split('|');
+    if (!teamString) {
+        console.log('data does not contain |', data);
+    } else {
+        teamString = teamString[indexToTake];
+    }
     
     if (!isHomeTeam) {
         teamString = teamString.split(' (')[0];
@@ -75,7 +87,12 @@ function parseTeam (data, isHomeTeam) {
         team.score = teamString.substring(teamString.length - 2);
     }
     
-    team.name = teamString.split(team.score)[0].trim().replace('^', '');
+    if (team.score && team.score > 0) {
+        team.name = teamString.split(team.score)[0].trim().replace('^', '');
+    } else {
+        team.score = 0;
+        team.name = teamString;
+    }
     team.score = parseInt(team.score);
     
     return team;
@@ -85,9 +102,11 @@ function determineGameWinner (game) {
     if (game.homeTeam.score > game.awayTeam.score) {
         game.homeTeam.isWinner = true;
         game.awayTeam.isWinner = false;
+    } else if (game.homeTeam.score === game.awayTeam.score) {
+        game.homeTeam.isWinner = false;
+        game.awayTeam.isWinner = false;
     } else {
         game.homeTeam.isWinner = false;
         game.awayTeam.isWinner = true;
     }
-    //TODO: determine how we want to display/indicate ties and then provide that data
 }
