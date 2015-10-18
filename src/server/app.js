a264c23e3e318500a4fc8b7dbe8f8d1faf6500fe
Clarkio <brian@clarkio.com>
2015-10-18 11:48:31 -0400

    todo - fix tests since it's not expecting xhr calls

--- src/server/app.js
+++ src/server/app.js
@@ -21,6 +21,7 @@ app.use(favicon(__dirname + '/favicon.ico'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
+app.set('view engine', 'html');
 
 io.on('connection', function (socket) {
     socket.emit('news', {hello: 'world'});
@@ -54,15 +55,19 @@ function checkNflScores () {
         .then(function (result) {
             var scoreChanges = scoreCalculator.determineScoreChanges(gameData, result);
             var scoreChangeAudio;
+            
             if (scoreChanges && scoreChanges.length > 0) {
                 gameData = result;
                 io.emit('NFL-ALL-UPDATE', result);
                 scoreChangeAudio = scoreAudio.determineScoreChangeAudio(scoreChanges);
+                
+                if (scoreChangeAudio && scoreChangeAudio.length > 0) {
+                    io.emit('NFL-SCORE-CHANGE', scoreChangeAudio);
+                }
             }
-            if (scoreChangeAudio && scoreChangeAudio.length > 0) {
-                io.emit('NFL-SCORE-CHANGE', scoreChangeAudio);
-            }
+            
             console.log(moment().format('MM-DD-YYYY h:mm:ss:SSS a') + ': end update of NFL data', '\n----------');
+            
             if (endInterval) {
                 clearInterval(scoresUpdateIntervalId);
             }
@@ -105,6 +110,15 @@ switch (environment){
         // Any deep link calls should return index.html
         // app.use('/*', express.static('./build/index.html'));
         break;
+    case 'production':
+        console.log('** PROD **');
+        app.use(express.static('../client/'));
+        app.use(express.static('../'));
+        app.use('/', express.static('../client/index.html'));
+        app.use('/bower_components/', express.static('../../bower_components'));
+        app.use('/src/client/app/', express.static('../client/app'));
+        app.use('/.tmp/', express.static('../client/styles'));
+        break;
     default:
         console.log('** DEV **');
         app.use(express.static('./src/client/'));
@@ -116,8 +130,6 @@ switch (environment){
         // });
         // Any deep link calls should return index.html
         // app.use('/*', express.static('./src/client/index.html'));
-        // app.use('/socket.io/*', 
-        //     express.static('./node_modules/socket.io/node_modules/socket.io-client/socket.io.js'))
         break;
 }
 
